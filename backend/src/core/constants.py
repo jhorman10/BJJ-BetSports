f1c5d2341b42ae3dd5d585adc00e558245546140
Jhorman Orozco <jsojhor@gmail.com>
2026-01-02 22:38:25 -0500

    chore: Remove 'E2' from `DEFAULT_LEAGUES` constant.

--- backend/src/core/constants.py
+++ backend/src/core/constants.py
@@ -3,4 +3,4 @@ Centralized constants for the BJJ-BetSports backend.
 """
 
 # Default leagues used for training and predictions
-DEFAULT_LEAGUES = ["E0", "E2", "SP1", "D1", "I1", "F1", "B1", "P1"]
+DEFAULT_LEAGUES = ["E0", "SP1", "D1", "I1", "F1", "B1", "P1"]
